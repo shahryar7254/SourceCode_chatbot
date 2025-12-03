125e83a78b4d3b739674f3c484fd01a819c9929b
shahryar7254 <shahryarshabeer@gmail.com>
2025-12-03 12:24:31 +0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import ChatBot from './components/chatbot'; // Import ChatBot
 
 function App() {
   return (
@@ -18,6 +19,9 @@ function App() {
           Learn React
         </a>
       </header>
+
+      {/* ChatBot Component */}
+      <ChatBot />
     </div>
   );
 }
